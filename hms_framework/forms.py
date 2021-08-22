c3d6f164857f3eb9903f8c497f975b078c0e1ea1
Bruno Quintana <bruno.quintana@gmail.com>
2021-08-22 17:14:53 +0100

    Fix ORM issue of circular dependencies

--- hms_framework/forms.py
+++ hms_framework/forms.py
@@ -10,10 +10,17 @@ class BookingForm(ModelForm):
         'type': 'date', 'class': 'form-control'}))
     date_to = forms.DateField(widget=forms.DateInput(attrs={
         'type': 'date', 'class': 'form-control'}))
-    room = forms.ChoiceField(choices=[(room.id, str(room)) for room in Room.objects.all()],
+    room = forms.ChoiceField(choices=[],
                              widget=Select(attrs={'class': 'form-control'}))
-    customer = forms.ChoiceField(choices=[(customer.id, str(customer)) for customer in Customer.objects.all()],
+    customer = forms.ChoiceField(choices=[],
                                  widget=Select(attrs={'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        choice_list = [(room.id, str(room)) for room in Room.objects.all()]
+        self.fields['room'].choices = choice_list
+        choice_list = [(customer.id, str(customer)) for customer in Customer.objects.all()]
+        self.fields['customer'].choices = choice_list
 
     class Meta:
         model = Booking
